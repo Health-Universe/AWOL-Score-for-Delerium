8db27a3ba601fe1fc248978723948978d7a7b5d8
Dan Caron <dancaron@gmail.com>
2023-04-08 16:05:15 -0400

    main.py

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ def calculate_delirium_risk_score(age, spells_world_backward, oriented_to_locati
 
 st.title("Delirium Risk Calculator")
 
+nursing_illness_severity_mapping = {
+    "Not ill": 0,
+    "Mildly ill": 0,
+    "Moderately ill": 1,
+    "Severely ill": 1,
+    "Moribund": 1
+}
+
+# Get the point value for the selected nursing illness severity level
+nursing_illness_severity_points = nursing_illness_severity_mapping[nursing_illness_severity]
+
 # User input for delirium risk score parameters
 age = st.selectbox("Age (<80 years or >=80 years)", [0, 1], format_func=lambda x: "<80 years" if x == 0 else ">=80 years")
 spells_world_backward = st.selectbox("Correctly spells 'world' backward (Yes or No)", [0, 1], format_func=lambda x: "Yes" if x == 0 else "No")
 oriented_to_location = st.selectbox("Oriented to city, state, county, hospital name, and floor (Yes or No)", [0, 1], format_func=lambda x: "Yes" if x == 0 else "No")
-nursing_illness_severity = st.selectbox("Nursing illness severity assessment (Not ill, Mildly ill, Moderately ill, Severely ill, Moribund)", [0, 0, 1, 1, 1], format_func=lambda x: "Not ill" if x == 0 else ("Mildly ill" if x == 0 else ("Moderately ill" if x == 1 else ("Severely ill" if x == 1 else "Moribund"))))
+nursing_illness_severity = st.radio(
+    "Nursing illness severity assessment",
+    options=list(nursing_illness_severity_mapping.keys())
+)
 
 # Calculate delirium risk score
 if st.button("Calculate Delirium Risk Score"):
